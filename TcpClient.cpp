7143c52ca6c54f5003389a25bd9553b7d1ee724d
norka <nordup.ondr@gmail.com>
2021-03-13 13:50:23 +0300

    header file

--- TcpClient.cpp
+++ TcpClient.cpp
@@ -1,30 +1,4 @@
-#include "Poco/Net/TCPServer.h"
-#include "Poco/Net/TCPServerConnection.h"
-#include "Poco/Net/TCPServerConnectionFactory.h"
-#include "Poco/Net/StreamSocket.h"
-#include "Poco/NumberParser.h"
-#include "Poco/Logger.h"
-#include "Poco/Process.h"
-#include "Poco/NamedEvent.h"
-#include <iostream>
-#include <thread>
-
-using Poco::Net::TCPServer;
-using Poco::Net::TCPServerConnectionFilter;
-using Poco::Net::TCPServerConnection;
-using Poco::Net::TCPServerConnectionFactory;
-using Poco::Net::TCPServerConnectionFactoryImpl;
-using Poco::Net::StreamSocket;
-using Poco::Net::SocketAddress;
-using Poco::UInt16;
-using Poco::NumberParser;
-using Poco::Logger;
-using Poco::Event;
-using Poco::NamedEvent;
-using Poco::Process;
-using Poco::ProcessImpl;
-using Poco::Exception;
-
+#include "header.hpp"
 
 namespace
 {
@@ -43,6 +17,9 @@ namespace
             socket.shutdown();
         }
 
+        /**
+         * Receive data and handle it
+         */
         void run()
         {
             try
--- TcpServer.cpp
+++ TcpServer.cpp
@@ -1,29 +1,4 @@
-#include "Poco/Net/TCPServer.h"
-#include "Poco/Net/TCPServerConnection.h"
-#include "Poco/Net/TCPServerConnectionFactory.h"
-#include "Poco/Net/StreamSocket.h"
-#include "Poco/NumberParser.h"
-#include "Poco/Logger.h"
-#include "Poco/Process.h"
-#include "Poco/NamedEvent.h"
-#include <iostream>
-
-
-using Poco::Net::TCPServer;
-using Poco::Net::TCPServerConnectionFilter;
-using Poco::Net::TCPServerConnection;
-using Poco::Net::TCPServerConnectionFactory;
-using Poco::Net::TCPServerConnectionFactoryImpl;
-using Poco::Net::StreamSocket;
-using Poco::UInt16;
-using Poco::NumberParser;
-using Poco::Logger;
-using Poco::Event;
-using Poco::NamedEvent;
-using Poco::Process;
-using Poco::ProcessImpl;
-using Poco::Exception;
-
+#include "header.hpp"
 
 namespace
 {
@@ -34,6 +9,9 @@ namespace
         {
         }
 
+        /**
+         * Receive data and handle it
+         */
         void run() override
         {
             StreamSocket& ss = socket();
